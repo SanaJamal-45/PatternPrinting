519d258f62c854653a3c0fc68821978eeb582f12
Sana Jamal <sanajamal869@gmail.com>
2024-03-22 12:21:26 +0500

    Add files via upload

---
+++ Pattern1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main() {
+	for (int i = 0;i <= 3;i++) {
+		for (int j = 0;j <= 4;j++) {
+			cout << "*" << " ";
+		}
+		cout << endl;
+	}
+	
+}
