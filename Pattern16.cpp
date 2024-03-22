b1058a80cfb87fbfe47fbf6db75019c03bfd072b
Sana Jamal <sanajamal869@gmail.com>
2024-03-22 15:08:42 +0500

    Add files via upload

---
+++ Pattern16.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main() {
+	int n;
+	cout << "Enter number of rows:";
+	cin >> n;
+	for (int i = 1;i <= n;i++) {
+		for (int j = 1;j <= 6;j++) {
+			if (i == 1 || i == n || j == 1 || j == 6){
+				cout << "*" << " ";
+			}
+			else {
+				cout << " " << " ";
+			}
+		}
+		cout << endl;
+	}
+}
