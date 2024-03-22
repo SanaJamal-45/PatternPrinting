1a7fe42db92219919d4f59f33538750e8b82c7de
Sana Jamal <sanajamal869@gmail.com>
2024-03-22 15:31:30 +0500

    Add files via upload

---
+++ Pattern18.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main() {
+	int n;
+	cout << "Enter number of rows:";
+	cin >> n;
+	for (int i = 1;i <= 4;i++) {
+		for (int j = 1;j <= n-i;j++) {
+			cout << " "<<" ";
+		}
+
+		for (int j = 1;j <=i;j++) {
+			cout <<"*"<<" ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
