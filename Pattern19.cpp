69aaf2c8778d6fa274c4952526a251d402dc2033
Sana Jamal <sanajamal869@gmail.com>
2024-03-22 15:37:08 +0500

    Add files via upload

---
+++ Pattern19.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main() {
+	int n;
+	cout << "Enter number of rows:";
+	cin >> n;
+	for (int i = 1;i <= 4;i++) {  //rows
+		for (int j = 1;j <= n - i;j++) {  //spaces
+			cout << " "<<" ";
+		}
+
+		for (int j = 1;j <= n;j++) {   //stars
+			cout << "*"<<" ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
