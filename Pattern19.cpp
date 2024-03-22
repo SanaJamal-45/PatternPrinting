4f42250f6efa3e49e9e5ea3fdb226210525f6ad5
Sana Jamal <sanajamal869@gmail.com>
2024-03-22 15:43:02 +0500

    Update Pattern19.cpp

--- Pattern19.cpp
+++ Pattern19.cpp
@@ -5,7 +5,7 @@ int main() {
 	int n;
 	cout << "Enter number of rows:";
 	cin >> n;
-	for (int i = 1;i <= 4;i++) {  //rows
+	for (int i = 1;i <= n;i++) {  //rows
 		for (int j = 1;j <= n - i;j++) {  //spaces
 			cout << " "<<" ";
 		}
